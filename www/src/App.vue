c978e7f9e0d1ff97a11453d8589c275da6cbd22c
Yannick <yannickhj@gmail.com>
2018-06-12 21:48:15 +0200

    frontend init/rest call working + rename test env

---
+++ www/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <service/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Service from './components/Service.vue'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Service
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
