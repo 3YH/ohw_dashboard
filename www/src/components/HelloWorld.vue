a827afad0395a8004a40d605c7d0ced8c192c731
Yannick <yannickhj@gmail.com>
2018-06-13 12:43:18 +0200

    vue router/sass support

--- www/src/components/HelloWorld.vue
+++ www/src/components/HelloWorld.vue
@@ -8,7 +8,6 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
@@ -39,7 +38,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
