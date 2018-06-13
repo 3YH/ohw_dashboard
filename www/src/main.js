6f2239428480c6782cc14a8479f40d3e6e5ce665
Yannick <yannickhj@gmail.com>
2018-06-13 15:52:10 +0200

    CORS fix/get data in table/vue-ui statistics
    Cross origins url fixes, loading data from REST into table via axios and now got some usage statistics in vue UI.

--- www/src/main.js
+++ www/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {Button, Table, TableColumn} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
 
+Vue.component(Button.name, Button);
+Vue.component(Table.name, Table);
+Vue.component(TableColumn.name, TableColumn);
+
+Object.defineProperty(Vue.prototype, '$axios', { value: axios });
 Vue.config.productionTip = false
 
 new Vue({
--- www/src/views/Home.vue
+++ www/src/views/Home.vue
@@ -2,6 +2,35 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <el-table
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column
+      prop="id"
+      label="id"
+      width="150">
+    </el-table-column>
+    <el-table-column
+      prop="locatienummer"
+      label="Locatienummer"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="soort"
+      label="Soort"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      fixed="right"
+      label="Operations"
+      width="120">
+      <template slot-scope="scope">
+        <el-button @click="handleClick" type="text" size="small">Detail</el-button>
+        <el-button type="text" size="small">Edit</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+
   </div>
 </template>
 
@@ -13,6 +42,30 @@ export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+      return {
+       tableData: [],
+       apiUrl: 'http://localhost:8090/api/objecten/',
+      }
+  },
+  mounted (){
+              this.getObjecten()
+              },
+    methods: {
+      handleClick() {
+        console.log('click');
+      },
+      getObjecten: function () {
+                this.$axios.get(this.apiUrl, {
+                }).then((response) => {
+                    this.tableData = response.data
+                    console.log(response.data);
+                })
+                 .catch(e => {
+                   this.errors.push(e)
+                   })
+            }
+    }
 }
 </script>
