8fea54a960296b0e108faf627fa4f8e9e084c18b
Yannick <yannickhj@gmail.com>
2018-06-19 13:01:09 +0200

    add/delete obj in frontend

--- www/src/components/ObjTable.vue
+++ www/src/components/ObjTable.vue
@@ -5,11 +5,11 @@
       </el-table-column>
       <el-table-column label="Locatie" type="expand" width="180px">
         <template slot-scope="props">
-           <p>Plaats: {{ props.row.plaats }}</p>
-           <p>Gemeente: {{ props.row.gemeente }}</p>
-           <p>Straatnaam: {{ props.row.straatnaam }}</p>
-           <p>X-Coördinaat: {{ props.row.xcord }}</p>
-           <p>Y-Coördinaat: {{ props.row.ycord }}</p>
+             <p>Plaats: {{ props.row.plaats }}</p>
+             <p>Gemeente: {{ props.row.gemeente }}</p>
+             <p>Straatnaam: {{ props.row.straatnaam }}</p>
+             <p>X-Coördinaat: {{ props.row.xcord }}</p>
+             <p>Y-Coördinaat: {{ props.row.ycord }}</p>
 </template>
       </el-table-column>
       <el-table-column prop="locatienummer" label="Nummer" aling="left">
@@ -32,36 +32,78 @@
       <el-table-column fixed="right" label="Operations">
 <template scope="scope">
   <vs-button @click="editObject(scope.$index, tableData)" vs-icon="edit"></vs-button>
+  <vs-button @click="getDelObject(scope.$index, tableData)" vs-icon="delete"></vs-button>
+  
 </template>
     </el-table-column>
   </el-table>
-   <el-dialog title="Object wijzigen" :visible.sync="dialogVisible" custom-class="popup">
-      <el-form :model="form">
+  <vs-button @click="addDialogVisible = true" vs-color="primary" vs-type="filled">Primary</vs-button>
+   <el-dialog title="Object wijzigen" :visible.sync="editDialogVisible" custom-class="popup">
+      <el-form :model="editform">
          <vs-tabs>
       <vs-tab vs-label="Onderhoud">
         <div class="con-tab-ejemplo">
-           <vs-input vs-label="Locatienummer" vs-placeholder="Placeholder" v-model="form.locatienummer"/>
+           <vs-input vs-label="Locatienummer" vs-placeholder="Placeholder" v-model="editform.locatienummer"/>
           <label>Onderhoud nodig?
-           <vs-switch vs-type="warning" vs-icon="build" v-model="form.onderhoud"/></label>
+           <vs-switch vs-type="warning" vs-icon="build" v-model="editform.onderhoud"/></label>
         </div>
       </vs-tab>
       <vs-tab vs-label="Locatie">
         <div class="con-tab-ejemplo">
-        <vs-input vs-label="Label" vs-placeholder="Placeholder" v-model="form.plaats"/>
+        <vs-input vs-label="Label" vs-placeholder="Placeholder" v-model="editform.plaats"/>
         </div>
       </vs-tab>
       <vs-tab vs-label="Inhoud">
         <div class="con-tab-ejemplo">
-        <vs-input vs-label="Label" vs-placeholder="Placeholder" v-model="form.plaats"/>
+        <vs-input vs-label="Label" vs-placeholder="Placeholder" v-model="editform.plaats"/>
         </div>
       </vs-tab>
     </vs-tabs>
         </el-form>
         <div slot="footer" class="dialog-footer">
-            <el-button :plain="true" type="danger" @click="dialogVisible = false">Cancel</el-button>
-            <el-button :plain="true" @click="updateForm(form)">Save</el-button>
+            <el-button :plain="true" type="danger" @click="editDialogVisible = false">Cancel</el-button>
+            <el-button :plain="true" @click="updateForm(editform)">Save</el-button>
         </div>
     </el-dialog>
+    <el-dialog
+  title="Object toevoegen"
+  :visible.sync="addDialogVisible" custom-class="popup">
+  <el-form :model="addform">
+         <vs-tabs>
+      <vs-tab vs-label="Onderhoud">
+        <div class="con-tab-ejemplo">
+           <vs-input vs-label="Locatienummer" vs-placeholder="Placeholder" v-model="addform.locatienummer"/>
+        </div>
+      </vs-tab>
+      <vs-tab vs-label="Locatie">
+        <div class="con-tab-ejemplo">
+        <vs-input vs-label="Label" vs-placeholder="Placeholder" v-model="addform.plaats"/>
+        </div>
+      </vs-tab>
+      <vs-tab vs-label="Inhoud">
+        <div class="con-tab-ejemplo">
+      
+        </div>
+      </vs-tab>
+    </vs-tabs>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button :plain="true" type="danger" @click="addDialogVisible = false">Cancel</el-button>
+            <el-button :plain="true" @click="newObjForm(addform)">Save</el-button>
+        </div>
+</el-dialog>
+<el-dialog
+  title="Tips"
+  :visible.sync="deleteDialogVisible"
+  width="30%">
+  <span>{{deleteobj.locatienummer}}</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">Cancel</el-button>
+    <el-button type="primary" @click="delObj(deleteobj)">Confirm</el-button>
+  </span>
+</el-dialog>
+
+
 </div>
 </template>
 <style lang="scss">
@@ -69,65 +111,63 @@
   .el-table th.is-leaf {
     border-bottom: none!important;
   }
-  .objtable{
-    width:75%!important;
-    margin:20px;
-    .el-table__expanded-cell{
-       background-color:transparent;
+  .objtable {
+    width: 75%!important;
+    margin: 20px;
+    .el-table__expanded-cell {
+      background-color: transparent;
     }
-  .el-table__body {
-    border-spacing: 0 1rem;
-    border-collapse: separate;
-    .styled-row {
-      border: 0px solid #fff;
-      
-      td:first-child {
-        border: 0px;
-        border-top-left-radius: 5px;
-        border-bottom-left-radius: 5px;
-      }
-      td:last-child {
-        border: 0px;
-        border-top-right-radius: 5px;
-        border-bottom-right-radius: 5px;
-      }
-      .cell {
-        text-align: left;
-        button.vs-btn.vs-button-primary-filled.filled.vs-button-icon{
-          background-color: #fff;
-          &:hover{
-            box-shadow:none;
-            span.text{
-               color:#409EFF;
-                opacity:1;
+    .el-table__body {
+      border-spacing: 0 1rem;
+      border-collapse: separate;
+      .styled-row {
+        border: 0px solid #fff;
+        td:first-child {
+          border: 0px;
+          border-top-left-radius: 5px;
+          border-bottom-left-radius: 5px;
+        }
+        td:last-child {
+          border: 0px;
+          border-top-right-radius: 5px;
+          border-bottom-right-radius: 5px;
+        }
+        .cell {
+          text-align: left;
+          button.vs-btn.vs-button-primary-filled.filled.vs-button-icon {
+            background-color: #fff;
+            &:hover {
+              box-shadow: none;
+              span.text {
+                color: #409EFF;
+                opacity: 1;
+              }
             }
-          }
-          span.text{
-            color:#000;
-          opacity:0.58;
+            span.text {
+              color: #000;
+              opacity: 0.58;
+            }
           }
         }
       }
+      .hover-row {
+        td {
+          background-color: rgba(64, 160, 255, 0.212)!important;
+        }
+      }
     }
-    .hover-row{
-      td{
-        background-color:#fff!important;
-        
-      }
-      }
   }
-  }
-  .el-dialog.popup{
+  .el-dialog.popup {
     border-radius: 5px;
     text-align: left;
-    .ul-tabs{
-      li{
+    .ul-tabs {
+      li {
         list-style: none;
       }
     }
-    .con-tab{
-      .con-tab-ejemplo{
-        label{
+    .con-tab {
+      .con-tab-ejemplo {
+        label {
           justify-content: left;
         }
       }
@@ -144,8 +184,12 @@
         actives: 2,
         reducex: false,
         tableData: [],
-        dialogVisible: false,
-        form: {},
+        addDialogVisible: false,
+        editDialogVisible: false,
+        deleteDialogVisible: false,
+        addform: {},
+        editform: {},
+        deleteobj: {},
       }
     },
     mounted() {
@@ -153,7 +197,7 @@
     },
     methods: {
       cellValueRenderer(row, column, cellValue) {
-      return  cellValue ? 'Ja' : 'Nee';
+        return cellValue ? 'Ja' : 'Nee';
       },
       filterTag(value, row) {
         return row.tag === value;
@@ -171,35 +215,69 @@
             this.errors.push(e)
           })
       },
+      newObjForm: function(addObjForm) {
+        let objId = addObjForm.id;
+        let locatienummer = addObjForm.locatienummer;
+        let plaats = addObjForm.plaats;
+        let onderhoud = addObjForm.onderhoud;
+        this.$axios.post('http://localhost:8090/api/objecten/', {
+            locatienummer: locatienummer,
+            plaats: plaats,
+            onderhoud: onderhoud,
+          })
+          .then(response => {})
+          .catch(function(error) {
+            console.log(error);
+          });
+        location.reload();
+      },
       editObject: function(index, rows) {
-        this.dialogVisible = true;
+        this.editDialogVisible = true;
         const objId = rows[index].id;
         const idurl = 'http://localhost:8090/api/objecten/' + objId;
         this.$axios.get(idurl).then((response) => {
-          this.form = response.data;
+          this.editform = response.data;
         }).catch(function(response) {
           console.log(response)
         });
       },
-      updateForm: function(formName) {
-        let objId = formName.id;
-        let locatienummer = formName.locatienummer;
-        let plaats = formName.plaats;
-        let onderhoud = formName.onderhoud;
+      updateForm: function(editObjForm) {
+        let objId = editObjForm.id;
+        let locatienummerr = editObjForm.locatienummer;
+        let plaats = editObjForm.plaats;
+        let onderhoud = editObjForm.onderhoud;
         this.$axios.put('http://localhost:8090/api/objecten/' + objId, {
             locatienummer: locatienummer,
             plaats: plaats,
-            onderhoud:onderhoud,
+            onderhoud: onderhoud,
           })
-          .then(function(response) {
-            console.log(response);
-            this.form = response.data;
-          })
+          .then(response => {})
           .catch(function(error) {
             console.log(error);
           });
-          location.reload();
+        location.reload();
       },
+       getDelObject: function(index, rows) {
+        this.deleteDialogVisible = true;
+        const objId = rows[index].id;
+        const idurl = 'http://localhost:8090/api/objecten/' + objId;
+        this.$axios.get(idurl).then((response) => {
+          this.deleteobj = response.data;
+        }).catch(function(response) {
+          console.log(response)
+        });
+      },
+      delObj: function(deleteObj) {
+        let objId = deleteObj.id;
+        this.$axios.delete('http://localhost:8090/api/objecten/' + objId, {
+            objId: objId,
+          })
+          .then(response => {})
+          .catch(function(error) {
+            console.log(error);
+          });
+        location.reload();
+      }
     }
   }
 </script>
